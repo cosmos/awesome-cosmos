d4478a6a1f5f8f1de7473d0a677d2ee807944970
Cyrus Goh <hello@lovincyrus.com>
2020-07-24 15:10:36 -0700

    re-order dir structure * all.json → projects/all * projects → projects/ * add a new script dir

--- dataset/concat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-jq -s . *.json > all.json
---
+++ dataset/script/concat.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if jq . ./../projects/*.json; then
+  # mkdir ./../projects/all
+  jq -s . ./../projects/*.json >> ./../projects/all/all.json
+  echo -e "\n Successfullly generated all.json in /projects/all 🎉\n"
+fi
