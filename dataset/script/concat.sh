74ab6c10a5ef682a4235e3734c6fc6db8f698741
Cyrus Goh <hello@lovincyrus.com>
2020-07-24 15:35:41 -0700

    fix appending multiple output, clean all.json

--- dataset/script/concat.sh
+++ dataset/script/concat.sh
@@ -2,6 +2,6 @@
 
 if jq . ./../projects/*.json; then
   # mkdir ./../projects/all
-  jq -s . ./../projects/*.json >> ./../projects/all/all.json
+  jq -s . ./../projects/*.json > ./../projects/all/all.json
   echo -e "\n Successfullly generated all.json in /projects/all 🎉\n"
 fi
